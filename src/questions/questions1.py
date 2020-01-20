302e94b8c8b4db63e61f7745bfc740d62f411765
Tom Nieuwland <thomasnieuwland19@gmail.com>
2020-01-20 20:37:26 +1100

    Added week 1 questions

---
+++ src/questions/questions1.py
@@ -0,0 +1,35 @@
+# Solve the following with a comment for the answer below
+
+# True or False is
+
+# True and True is
+
+# (True and False) or True is
+
+# Not False is
+
+# 1 or 0 is
+
+# not(False or False) and (True or False) is
+
+# True ^ True is
+
+# (True or False) and 3 > 2 is
+
+
+# Name each of the operators and explain what they do
+
+
+
+
+
+
+
+
+# Why do we use if/else/elif statements
+
+
+
+
+
+# In your own words why do we gather requirements
