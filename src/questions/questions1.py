79a9576b2f68cba2240a135a28f509d57225efc7
Tom Nieuwland <thomasnieuwland19@gmail.com>
2020-01-22 23:08:11 +1100

    q1 feedback

--- src/questions/questions1.py
+++ src/questions/questions1.py
@@ -9,7 +9,7 @@
 # Not False is
 # true
 # 1 or 0 is
-# 1
+# 1                           :: Technicaly 1 or 0 is True because a boolean statement can only solve to True or False HOWEVER 1 == True mathematically so this is fine
 # not(False or False) and (True or False) is
 # true 
 # True ^ True is
@@ -24,22 +24,26 @@
 # >= greater than or equal to (eg 3>=3)
 # <= less than or equal to (eg 3<=3)
 # != not equal to (eg 4 !=2)
-# == = compares 2 things and gives true is they are exactly the same (eg true == true solves to {true == true})
-
-
-
-
-
-
-
+# == = compares 2 things and gives true is they are exactly the same (eg true == true solves to {true == true})     :: Technically just solves to True because True == True
 
 # Why do we use if/else/elif statements
+
 # We use if/else/elif statements due to Python using boolean to determine wether things are true or false. 
 # The use of if/eles/elif statements are a safeguard against possible bugs, for example unintended data value inputs.
 
+# :: While these statements are true, if/else/elif are actually used to control the flow of code, only executing particular segments when boolean conditions are fufilled
+# :: This, in turn, safeguards against bugs, but thats not the only reason we use if/else/elif
 
 
 # In your own words why do we gather requirements
+
 # In an ideal workforce, gathering requirements should be left to a competant project manager. 
 # However, most of the time the programmer does not live in an ideal workforce, thus it is important to have the skills necesarry to gather requirements independantly.
 # Furthermore, having an adept sense of what is and what isn't needed for a project can assist a better outcome for the task as a whole.
+
+# :: The first two sentences here are filler and don't actually answer the question, rather, they comment on the place of a project manager and what a good workforce looks
+# :: like. In reality, the reason we gather requirements is so that we have the most strictly defined understanding of what the client needs and wants, as well as a strictly
+# :: defined outline of the purpose of the software and why we are creating it in the first place. Requirements also include things like use cases, which are stories that
+# :: describe how the customer expects to interact with the software. Requirements also contain constraints such as budgets, timelines, due dates, technology restraints etc etc.
+# :: There is much more to why we gather requirements than just getting a better outcome, in short its to achieve the _best_ outcome.
+
