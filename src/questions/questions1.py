d34fac95a4cd9e33804d20ad487ed89076ee9683
danny <voltorbg@gmailcom>
2020-01-22 22:56:26 +1100

    q1 answers
    gamings

--- src/questions/questions1.py
+++ src/questions/questions1.py
@@ -1,23 +1,30 @@
 # Solve the following with a comment for the answer below
 
 # True or False is
-
+# true 
 # True and True is
-
+# true
 # (True and False) or True is
-
+# true
 # Not False is
-
+# true
 # 1 or 0 is
-
+# 1
 # not(False or False) and (True or False) is
-
+# true 
 # True ^ True is
-
+# false
 # (True or False) and 3 > 2 is
-
+# true
 
 # Name each of the operators and explain what they do
+
+# > greater than (eg 5 > 3 = true)
+# < less than (eg 3 < 5 = true)
+# >= greater than or equal to (eg 3>=3)
+# <= less than or equal to (eg 3<=3)
+# != not equal to (eg 4 !=2)
+# == = compares 2 things and gives true is they are exactly the same (eg true == true solves to {true == true})
 
 
 
@@ -27,9 +34,12 @@
 
 
 # Why do we use if/else/elif statements
-
-
+# We use if/else/elif statements due to Python using boolean to determine wether things are true or false. 
+# The use of if/eles/elif statements are a safeguard against possible bugs, for example unintended data value inputs.
 
 
 
 # In your own words why do we gather requirements
+# In an ideal workforce, gathering requirements should be left to a competant project manager. 
+# However, most of the time the programmer does not live in an ideal workforce, thus it is important to have the skills necesarry to gather requirements independantly.
+# Furthermore, having an adept sense of what is and what isn't needed for a project can assist a better outcome for the task as a whole.
